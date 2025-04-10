0d159269c5a4e6bd4f028bd3812b08147530f562
Kalle <kalle@kandru.de>
2025-04-10 13:47:13 +0200

    bump version to 0.0.3

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace FunnyHostages
 {
     public partial class FunnyHostages
     {
-        public override string ModuleVersion => "0.0.2";
+        public override string ModuleVersion => "0.0.3";
     }
 }
